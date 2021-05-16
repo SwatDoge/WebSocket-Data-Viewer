8c1bf4d7df4fb2954cf784809a8494f93981f4e0
Stephan <bob.dedebouwer@hotmail.com>
2021-05-16 23:38:17 +0200

    Patched v2

--- script.user.js
+++ script.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         WebSocket Data Viewer
+// @name         Websocket Data Viewer
 // @namespace    https://github.com/Lemons1337/WebSocket-Data-Viewer
 // @version      0.1.2
 // @description  try to take over the world!
@@ -9,10 +9,9 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/msgpack-lite/0.1.26/msgpack.min.js
 // @grant        none
 // ==/UserScript==
-
 window.msgpack = msgpack;
 
-let websocket_blacklist = ["t", "gmsg", "ts", "k", "l", "pi", "pir"];
+let websocket_blacklist = ["t", "pi", "po", "0", "7", "pir", "9"];
 
 function unpack(data) {
 	if (data instanceof DataView) data = new Uint8Array(data.buffer);
@@ -23,29 +22,35 @@ function unpack(data) {
 }
 
 function websocket_replace(packet, dir){
-	console.log(packet, dir);
-    	if (dir === "down" && packet[0] === "sb") packet[1] = ["All hail DQ", "Beep boop", "Made by SwatDoge", "Join the game to get started"][Math.floor(Math.random() * 4)]; return packet;
-	
-	return packet;
+    if (dir === "down" && packet[0] === "sb"){ packet[1] = ["All hail DQ", "Beep boop", "Made by SwatDoge", "Join the game to get started"][Math.floor(Math.random() * 4)]; return packet};
+    if (dir === "up" && packet[0] === "a"){ packet = ["po"]; return packet };
+    return packet
+}
+
+function packet_modify(packet, dir){
+    let unpacked_data = unpack(packet), last_bytes = new Uint8Array(packet).slice(-2);
+    if (websocket_blacklist.includes(unpacked_data[0])) return packet;
+    unpacked_data = websocket_replace(unpacked_data, dir);
+
+    console.log(unpacked_data, dir);
+
+    let l = new Uint8Array([...msgpack.encode(unpacked_data), ...last_bytes]);
+
+    return l;
 }
 
 window.WebSocket = class extends window.WebSocket {
     constructor(...args){
         super(...args);
-        this.addEventListener("open", event => {
-            let incoming = this.onmessage, outgoing = this.send;
-            function packet_modify(packet, dir){
-                let unpacked_data = unpack(packet[0].data ? packet[0].data : packet[0]), last_bytes = new Uint8Array(packet[0].data).slice(-2);
-                if (websocket_blacklist.includes(unpacked_data[0])) return packet;
-                unpacked_data = websocket_replace(unpacked_data, dir);
-
-                let l = new Uint8Array([...msgpack.encode(unpacked_data), ...last_bytes]);
-                Object.defineProperty(packet[0], "data", {get: () => l.buffer});
-                return packet;
-            }
-
-            this.onmessage = function(){return incoming.apply(this, packet_modify(arguments, "down"));}
-            this.send = function(){return outgoing.apply(this, packet_modify(arguments, "up"));}
+        this.addEventListener("message", event => {
+            Object.defineProperty(event, 'data', {value: packet_modify(event.data, "down")});
         });
+        this.addEventListener("open", event => {
+            let send = this.send;
+            this.send = function(){
+                arguments[0] = packet_modify(arguments[0], "up");
+                return send.apply(this, arguments);
+            }
+        })
     }
 };
